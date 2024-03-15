2cba07fb28f0a3aea7cb7a937031542f1ef2064e
luiz.reboucas <luiz.reboucas@hostweb.com.br>
2024-03-15 13:09:30 -0300

    Response Entity

--- src/main/java/com/medi/api/controllers/MedicoController.java
+++ src/main/java/com/medi/api/controllers/MedicoController.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/medicos")
@@ -27,38 +29,45 @@ public class MedicoController {
     private MedicoRepository repository;
     @PostMapping
     @Transactional
-    public Medico createMedico(@RequestBody @Valid DadosCadastroMedicoDTO dados){
+    public ResponseEntity createMedico(@RequestBody @Valid DadosCadastroMedicoDTO dados, UriComponentsBuilder uriBuilder){
         Medico medico = new Medico(dados);
         repository.save(medico);
-        return medico;
+        var uri = uriBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoMedicoDTO(medico));
     }
 
     @GetMapping
-    public Page<DadosListagemMedicoDTO> getMedicos(Pageable paginacao){
-        return repository
+    public ResponseEntity<Page<DadosListagemMedicoDTO>> getMedicos(Pageable paginacao){
+        var page = repository
                 .findAllByAtivoTrue(paginacao)
                 .map(DadosListagemMedicoDTO::new);
-    }
-
-    @DeleteMapping("/{id}")
-    @Transactional
-    public void deleteMedico(@PathVariable Long id){
-        Medico medico = repository.getReferenceById(id);
-        medico.desativar();
+        return ResponseEntity.ok(page);
     }
 
     @GetMapping("/{id}")
-    @Transactional
-    public Medico getMedico(@PathVariable Long id) throws Exception{
-        Optional<Medico> optionalMedico= repository.findById(id);
-        if(optionalMedico.isEmpty()) throw new Exception("Médico não encontrado");
-        return optionalMedico.get();
+    public ResponseEntity<DadosDetalhamentoMedicoDTO> getMedico(@PathVariable Long id){
+        var medico = repository.getReferenceById(id);
+        if(!medico.getAtivo()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(new DadosDetalhamentoMedicoDTO(medico));
     }
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity deleteMedico(@PathVariable Long id){
+        Medico medico = repository.getReferenceById(id);
+        medico.desativar();
+        return ResponseEntity.noContent().build();
+    }
+
+
 
     @PutMapping
     @Transactional
-    public void updateMedico(@RequestBody MedicoUpdateDTO medicoAtualizado){
+    public ResponseEntity updateMedico(@RequestBody MedicoUpdateDTO medicoAtualizado){
         var medico = repository.getReferenceById(medicoAtualizado.id());
         medico.update(medicoAtualizado);
+        DadosDetalhamentoMedicoDTO dadosDetalhamento = new DadosDetalhamentoMedicoDTO(medico);
+        return ResponseEntity.ok(dadosDetalhamento);
     }
 }
---
+++ src/main/java/com/medi/api/medico/DadosDetalhamentoMedicoDTO.java
@@ -0,0 +1,10 @@
+package com.medi.api.medico;
+
+import com.medi.api.endereco.Endereco;
+
+public record DadosDetalhamentoMedicoDTO(Long id, String nome, String email, String crm, Especialidade especialidade, Endereco endereco) {
+
+    public DadosDetalhamentoMedicoDTO(Medico medico){
+        this(medico.getId(),medico.getNome(), medico.getEmail(),medico.getCrm(), medico.getEspecialidade(),medico.getEndereco());
+    }
+}
